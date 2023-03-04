6368340722e905f1dd993e378c2e8d64541a6b76
caioliveirap <caio191201@gmail.com>
2023-03-04 01:05:57 -0300

    feat: added the sidenav to the project

---
+++ src/app/sidenav/NavbarData.ts
@@ -0,0 +1,27 @@
+export const navbarData = [
+  {
+    routeLink: "dashboard",
+    icon_src: "assets/ailos_icons/search.svg",
+    label: "Ailos",
+  },
+  {
+    routeLink: "products",
+    icon_src: "assets/ailos_icons/star.svg",
+    label: "Ailos",
+  },
+  {
+    routeLink: "statistics",
+    icon_src: "assets/ailos_icons/message.svg",
+    label: "Ailos",
+  },
+  {
+    routeLink: "statistics",
+    icon_src: "assets/ailos_icons/filter.svg",
+    label: "Ailos",
+  },
+  {
+    routeLink: "statistics",
+    icon_src: "assets/ailos_icons/temple.svg",
+    label: "Ailos",
+  },
+];
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,64 @@
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  OnInit,
+  HostListener,
+} from '@angular/core';
+import { navbarData } from './NavbarData';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent implements OnInit {
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
+  navData = navbarData;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({
+        collapsed: this.collapsed,
+        screenWidth: this.screenWidth,
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    console.log(navbarData);
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+}
