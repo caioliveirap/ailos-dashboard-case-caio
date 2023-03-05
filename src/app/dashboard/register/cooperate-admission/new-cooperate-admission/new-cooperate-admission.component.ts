effdbf570a2fbd60ba617697efcebc20bde25e64
caioliveirap <caio191201@gmail.com>
2023-03-05 00:48:08 -0300

    feat: all validations included and now the input shows an alert if error

--- src/app/dashboard/register/cooperate-admission/new-cooperate-admission/new-cooperate-admission.component.ts
+++ src/app/dashboard/register/cooperate-admission/new-cooperate-admission/new-cooperate-admission.component.ts
@@ -11,28 +11,44 @@ export class NewCooperateAdmissionComponent {
   cpfConsultationValue: string = '';
   selectedCooperate: CooperatesInterface | undefined;
   isLoadingCooperate: boolean = false;
+  isConsultationAvailable: boolean = true;
+  invalidCPF: boolean = false;
 
   validateCPF(cpf: string): boolean {
     // NOTE: This validation is limited because all the CPF's are fake.
     cpf = cpf.replace(/[^\d]+/g, '');
 
     if (cpf.length !== 11) {
+      this.invalidCPF = true;
       return false;
     }
 
+    this.invalidCPF = false;
     return true;
   }
 
   searchCooperateWithCPF(): void {
     // NOTE: This setTimeout is just to simulate the loading of the search.
-    console.log(this.cpfConsultationValue);
+    if (
+      !this.isConsultationAvailable ||
+      !this.validateCPF(this.cpfConsultationValue)
+    ) {
+      return;
+    }
     this.isLoadingCooperate = true;
+
     setTimeout(() => {
-      this.selectedCooperate = registeredCooperates.find(
+      const selectedCooperate = registeredCooperates.find(
         (cooperate: CooperatesInterface) => {
           return cooperate.cooperateCPF === this.cpfConsultationValue;
         }
       );
+
+      if (selectedCooperate) {
+        this.selectedCooperate = selectedCooperate;
+        this.isConsultationAvailable = false;
+      }
+
       this.isLoadingCooperate = false;
     }, 2000);
   }
